51779df80f432fdca1cac057899376a3ab36e374
4ZUK <60348334+4ZUK@users.noreply.github.com>
2020-02-14 17:53:04 -0300

    refaz dif

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,17 @@ public class Main {
     public static void main(String[] args) {
         Cachorro doido = new Cachorro();
         Cachorro toto = new Cachorro();
-
         toto.correr();
         doido.comer();
+
+        toto.cor = "azul";
+        System.out.println(toto.cor);
+        doido.tamanho = "Grande";
+        System.out.println(doido.tamanho);
+        doido.raca = "vira lata";
+        System.out.println(doido.raca);
+
+
+
     }
 }
