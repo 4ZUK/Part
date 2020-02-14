cff6a39f782b68adf2687795774d9d96e676aa21
4ZUK <60348334+4ZUK@users.noreply.github.com>
2020-02-14 17:24:04 -0300

    INOFF

---
+++ src/Cachorro.java
@@ -0,0 +1,25 @@
+public class Cachorro {
+    public String cor;
+    public String raca;
+    public String tamanho;
+
+    public void correr() {
+        System.out.println("Correndo...");
+    }
+
+
+    public void latir() {
+        System.out.println("Latindo....");
+    }
+
+    public void comer() {
+        System.out.println("Comendo...");
+    }
+
+
+
+}
+
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Cachorro doido = new Cachorro();
+        Cachorro toto = new Cachorro();
+
+        toto.correr();
+        doido.comer();
+    }
+}
